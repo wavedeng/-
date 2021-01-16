5e6f4239e2c078effcb0c58857f6be3ef67dd5cf
美丽的老邓 <wavedeng@yeah.net>
2021-01-16 20:14:56 +0800

    Update and rename main.py to get_fans.py

--- get_fans.py
+++ get_fans.py
@@ -2,7 +2,7 @@ import requests,re
 from http.cookies import SimpleCookie
 
 #这里填上你登陆时的cookie，下面有讲解如何获取
-cookies = '''_uuid=5DB6F12A-C1A6-4772-45D0-0F82156E57BF99336infoc; buvid3=DACD2C06-965A-4445-9029-20DCFA328434143094infoc; sid=c4aii2qv; CURRENT_FNVAL=80; blackside_state=1; rpdid=|(k)~Rk)JJY)0J'uY|)umumJm; fingerprint3=21d953ea5d483442256bacc0a9f0ddc7; buivd_fp=DACD2C06-965A-4445-9029-20DCFA328434143094infoc; buvid_fp_plain=DACD2C06-965A-4445-9029-20DCFA328434143094infoc; fingerprint=062594a4b2a4559bd304086a123e23a6; LIVE_BUVID=AUTO7416085217882235; dy_spec_agreed=1; buvid_fp=DACD2C06-965A-4445-9029-20DCFA328434143094infoc; bp_t_offset_599021380=478266950833678670; bp_video_offset_599021380=478190904144348234; CURRENT_QUALITY=116; DedeUserID=277091524; DedeUserID__ckMd5=83902bca2989f6ac; SESSDATA=37d37db0%2C1626143982%2C0a9a9*11; bili_jct=1b0ce455aae35b70c45bc5973302e152; fingerprint_s=0510db5feb41aff108b6801637d882dd; bp_video_offset_277091524=480147102836298343; bp_t_offset_277091524=480147102836298343; PVID=1'''
+cookies = '''id3=DACD2C06-965A-4445-9029-20DCFA32d953ea5d483442256-965A-4445-902bca2989f6ac; SESS73302e152; fingerprint_s=0510db5feb41aff108b6801637d882dd; bp_video_offset_277091524=480147102836298343; bp_t_offset_277091524=480147102836298343; PVID=1'''
 cookie = SimpleCookie(cookies)
 cookies = {coo.key:coo.value for coo in cookie.values()}
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36"}
